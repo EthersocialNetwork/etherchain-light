93d97bdb38e061c0df43a00e2fc7270041fa81e2
KimSejun <cent@naver.com>
2018-07-05 08:39:55 +0900

    time processing SVN rev.3~18

--- routes/top100.js
+++ routes/top100.js
@@ -5,8 +5,8 @@ var BigNumber = require('bignumber.js');
 var async = require('async');
 var redis = require("redis"),
 	client = redis.createClient();
-var max = 0, min = 1500;
-var redis_args = [ 'esn', max, min, 'WITHSCORES' ];
+var max = 0, min = 1000;
+var redis_args = [ 'esn_top100', max, min, 'WITHSCORES' ];
 
 router.get('/:offset?', function(req, res, next) {
 	var config = req.app.get('config');
@@ -24,11 +24,17 @@ router.get('/:offset?', function(req, res, next) {
 		function(accounts, callback) {
 			var data_special = {};
 			var data_normal = {};
+			var create_time = "";
 			var idx = 0;
-			var rank_normal = 1;
+			var rank_normal = 0;
 			var rank_special = 1;
 			async.eachSeries(accounts, function(account, eachCallback) {
-				if(rank_normal < 1001) {
+				if(rank_normal === 0)	{
+					create_time = accounts[idx];
+					idx = idx + 2;
+					rank_normal++
+				}
+				if(rank_normal < 501)	{
 					if (config.names[accounts[idx]]) {
 						data_special[accounts[idx]] = {};
 						data_special[accounts[idx]].address = accounts[idx];
@@ -48,12 +54,12 @@ router.get('/:offset?', function(req, res, next) {
 				}
 				eachCallback();
 			}, function(err) {
-				callback(err, data_special, data_normal);
+				callback(err, create_time, data_special, data_normal);
 			});
 		}
 	], 
-	function(err, accounts_special, accounts_normal) {
-		res.render("top100", { "accounts_special": accounts_special, "accounts_normal": accounts_normal });
+	function(err, accounts_create_time, accounts_special, accounts_normal) {
+		res.render("top100", { "accounts_create_time": accounts_create_time, "accounts_special": accounts_special, "accounts_normal": accounts_normal });
 	});
 });
 
--- routes/top100_settingup.js
+++ routes/top100_settingup.js
@@ -5,69 +5,86 @@ var BigNumber = require('bignumber.js');
 var async = require('async');
 var Web3 = require('web3');
 var redis = require("redis"),
-    client = redis.createClient();
+client = redis.createClient();
 
 router.get('/:offset?', function(req, res, next) {
-  var config = req.app.get('config');  
-  var web3 = new Web3();
-  var Ether     = new BigNumber(10e+17);
-  web3.setProvider(config.provider);
-  
-  client.on("error", function (err) {
-    console.log("Error " + err);
-  });
- 
-  async.waterfall([
-    function(callback) {
-      web3.parity.listAccounts(1000000, req.params.offset, function(err, result) {
-        callback(err, result);
-      });
-    }, function(accounts, callback) {
-      
-      var data = {};
-      
-      if (!accounts) {
-        return callback({name:"FatDBDisabled", message: "Parity FatDB system is not enabled. Please restart Parity with the --fat-db=on parameter."});
-      }
-      
-      if (accounts.length === 0) {
-        return callback({name:"NoAccountsFound", message: "Chain contains no accounts."});
-      }
-      
-      var lastAccount = accounts[accounts.length - 1];
-      
-      async.eachSeries(accounts, function(account, eachCallback) {
-        web3.eth.getCode(account, function(err, code) {
-          if (err) {
-            return eachCallback(err);
-          }
-          data[account] = {};
-          data[account].address = account;
-          data[account].type = code.length > 2 ? "Contract" : "Account";
-          
-          web3.eth.getBalance(account, function(err, balance) {
-            if (err) {
-              return eachCallback(err);
-            }
-            data[account].balance = balance;
-            var numBalance = new BigNumber(balance);
-            numBalance = numBalance.dividedBy(Ether);
-            if(code.length < 3 && numBalance > 0) {
-                client.zadd('esn',numBalance,account);
-            }
-            eachCallback();
-          });
-        });
-      }, function(err) {
-        callback(err, data, lastAccount);
-      });
-    }
-  ], function(err, accounts, lastAccount) {
-    if (err) {
-      return next(err);
-    }
-    res.render("top100", { accounts: accounts });
-  });
+	var config = req.app.get('config');  
+	var web3 = new Web3();
+	var Ether     = new BigNumber(10e+17);
+	web3.setProvider(config.provider);
+
+	client.on("error", function (err) {
+		console.log("Error " + err);
+	});
+
+	async.waterfall([
+		function(callback) {
+			web3.parity.listAccounts(1000000, req.params.offset, function(err, result) {
+				callback(err, result);
+			});
+		}, 
+		function(accounts, callback) {
+			client.del('esn_top100');
+			if (!accounts) {
+				return callback({name:"FatDBDisabled", message: "Parity FatDB system is not enabled. Please restart Parity with the --fat-db=on parameter."});
+			}
+			if (accounts.length === 0) {
+				return callback({name:"NoAccountsFound", message: "Chain contains no accounts."});
+			}
+			async.eachSeries(accounts, function(account, eachCallback) {
+				web3.eth.getCode(account, function(err, code) {
+					if (err) {
+						return eachCallback(err);
+					}
+					web3.eth.getBalance(account, function(err, balance) {
+						if (err) {
+							return eachCallback(err);
+						}
+						var numBalance = new BigNumber(balance);
+						numBalance = numBalance.dividedBy(Ether);
+						if(code.length < 3 && numBalance > 0) {
+							client.zadd('esn_top100',numBalance,account);
+						}
+						eachCallback();
+					});
+				});
+			}, function(err) {
+				callback(err, "Error");
+			});
+
+			client.zadd('esn_top100','2100000000', printDateTime());
+		}
+	], function(err, printdatetime) {
+		if (err) {
+			return next(err);
+		}
+		res.render("top100_settingup", { "printdatetime": printdatetime });
+	});
 });
 
 module.exports = router;
+
+function addZeros(num, digit) { // 자릿수 맞춰주기
+	  var zero = '';
+	  num = num.toString();
+	  if (num.length < digit) {
+		for (i = 0; i < digit - num.length; i++) {
+		  zero += '0';
+		}
+	  }
+	  return zero + num;
+}
+
+function printDateTime() {
+			var currentDate = new Date();                                     // 현재시간
+			var calendar = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate() // 현재 날짜
+			var amPm = 'AM'; // 초기값 AM
+			var currentHours = addZeros(currentDate.getHours(),2); 
+			var currentMinute = addZeros(currentDate.getMinutes(),2);
+			var currentSeconds =  addZeros(currentDate.getSeconds(),2);
+			if(currentHours >= 12){ // 시간이 12보다 클 때 PM으로 세팅, 12를 빼줌
+				amPm = 'PM';
+				currentHours = addZeros(currentHours - 12,2);
+			}
+			return calendar +" "+currentHours+":"+currentMinute+":"+currentSeconds+" "+amPm; //날짜를 출력해 줌
+}
