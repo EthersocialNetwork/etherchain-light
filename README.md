25a57050eae5903080712debdadb446e02c909b4
KimSejun <cent@naver.com>
2018-07-13 15:12:15 +0900

    SVN rev.961~1272 - Source Cleaning - README.md update - implemented Block Chain Summary(Calculated in the All blocks) - apply redis multi exec - Implement donut-pie chart on 'peers geocharts' - tx bug exception handling

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Etherchain Light is an Ethereum blockchain explorer built with NodeJS, Express a
 
 While there are several excellent Ethereum blockchain explorers available (etherscan, ether.camp and etherchain) they operate on a fixed subset of Ethereum networks, usually the mainnet and testnet. Currently there are no network agnostic blockchain explorers available. If you want to develop Dapps on a private testnet or would like to launch a private / consortium network, Etherchain Light will allow you to quickly explore such chains.
 
-A demo instance connected to the Kovan Ethereum testnet is available at [light.etherchain.org](http://light.etherchain.org). An example of a verified contract source can be found at [0x0cf37d2d45427a1380db12c9b352d6f083143817](https://light.etherchain.org/account/0x0cf37d2d45427a1380db12c9b352d6f083143817). An example of a transaction where the corresponding Solidity function name and parameters have been identified can be found at [0x82da63f3d998415b748111e6f1d11051167fb995fdca990acd3cfd5a8b397c20](https://light.etherchain.org/tx/0x82da63f3d998415b748111e6f1d11051167fb995fdca990acd3cfd5a8b397c20)
+A demo instance connected to the EtherSocialNetwork is available at [http://explorer.sejun.info](http://http://explorer.sejun.info).
 
 ## Current Features
 * Browse blocks, transactions, accounts and contracts
@@ -43,39 +43,17 @@ Supported Ethereum backend nodes: Parity (Geth is currently not supported as it 
 
 1. Setup a nodejs & npm environment
 2. Install the latest version of the Parity Ethereum client
-3. Start parity using the following options: `parity --chain=<yourchain> --tracing=on --fat-db=on --pruning=archive`
-4. Clone this repository to your local machine: `git clone https://github.com/gobitfly/etherchain-light --recursive` (Make sure to include `--recursive` in order to fetch the solc-bin git submodule)
+3. Start parity using the following options: `./parity --datadir ../../chaindata/ --chain ethersocial --port 50505 --mode active --ipc-path ~/.Ethersocial/gesc.ipc --fat-db=on --pruning=archive --author 0x5811590907050746b897eFE65FeA7B65710E1a2c --max-peers 15`
+4. Clone this repository to your local machine: `git clone https://github.com/EthersocialNetwork/etherchain-light --recursive` (Make sure to include `--recursive` in order to fetch the solc-bin git submodule)
 5. Install all dependencies: `npm install`
 6. Rename `config.js.example` into `config.js` and adjust the file to your local environment
 7. Start the explorer: `npm start`
 8. Browse to `http://localhost:3000`
 
-### Setup using docker
-
-Build then run the container
-```bash
-docker build -t etherchain-light .
-docker run -p 3000:3000 etherchain-light
-```
-
-Or directly bind the config.js file to avoid rebuilding the image
-```bash
-docker run -p "3000:3000" \
-    -v "$(pwd)/config.js":/usr/src/app/config.js \
-    etherchain-light
-```
-
-### Setup using docker-compose
-
-```bash
-docker-compose up
-```
 
 ```crontab
-#   no IP Check
 0 */2 * * * curl 'http://explorer.sejun.info/top100_settingup'
-
-#   IP Check
 */10 * * * * curl 'http://explorer.sejun.info/peers'
 */1 * * * * curl 'http://explorer.sejun.info/redisblock'
+7 */2 * * * curl 'http://explorer.sejun.info/hashratechart'
 ```
---
+++ routes/hashratechart.js
@@ -0,0 +1,313 @@
+var express = require('express');
+var router = express.Router();
+var events = require("events");
+events.EventEmitter.prototype._maxListeners = 100;
+var async = require('async');
+var Web3 = require('web3');
+const redis = require("redis");
+const client = redis.createClient();
+const pre_fix = 'explorerBlocks:';
+const pre_fix_chart = 'explorerBlocksChart:';
+const divide = 10000;
+
+router.get('/', function (req, res, next) {
+  var config = req.app.get('config');
+  var web3 = new Web3();
+  web3.setProvider(config.provider);
+  var data = {};
+  var tmpData = {};
+  tmpData.BlockTime = [];
+  tmpData.Difficulty = [];
+  tmpData.NetHashrate = [];
+
+  var dbSaveDatas = {};
+  data.startTime = new Date();
+  data.ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  data.dbLastBlock = 0;
+  data.dbChartLastBlock = 0;
+  data.blockCount = 1000;
+
+  data.xData = [];
+  data.xBlocknumber = [];
+  data.xNumberOfBlocks = [];
+
+  data.lastnumber = 0;
+  data.datasets = [];
+  data.datasets[0] = {
+    "name": "BlockTime",
+    "data": [],
+    "unit": " s",
+    "type": "line",
+    "valueDecimals": 2
+  };
+  data.datasets[1] = {
+    "name": "Difficulty",
+    "data": [],
+    "unit": " TH",
+    "type": "area",
+    "valueDecimals": 4
+  };
+  data.datasets[2] = {
+    "name": "NetHashrate",
+    "data": [],
+    "unit": " GH/s",
+    "type": "line",
+    "valueDecimals": 2
+  };
+
+  client.on("error", function (err) {
+    console.log("Error " + err);
+  });
+
+  async.waterfall([
+    function (callback) {
+      client.hget(pre_fix.concat("lastblock"), "lastblock", function (err, result) {
+        data.dbLastBlock = Number(result);
+        callback(err);
+      });
+    },
+    function (callback) {
+      client.hget(pre_fix_chart.concat("lastblock"), "lastblock", function (err, result) {
+        data.dbChartLastBlock = Number(result);
+        callback(err);
+      });
+    },
+    function (callback) {
+      client.lrange(pre_fix_chart.concat("xData"), 0, -1, function (err, result) {
+        function numberingArrayElements(element, index, array) {
+          data.xData.push(Number(element));
+        }
+        result.forEach(numberingArrayElements);
+        //console.log("data.xData: " + data.xData.length + " / " + result.length);
+        callback(err);
+      });
+    },
+    function (callback) {
+      client.lrange(pre_fix_chart.concat("xBlocknumber"), 0, -1, function (err, result) {
+        function numberingArrayElements(element, index, array) {
+          data.xBlocknumber.push(Number(element));
+        }
+        result.forEach(numberingArrayElements);
+        //console.log("data.xBlocknumber: " + data.xBlocknumber.length + " / " + result.length);
+        callback(err);
+      });
+    },
+    function (callback) {
+      client.lrange(pre_fix_chart.concat("xNumberOfBlocks"), 0, -1, function (err, result) {
+        function numberingArrayElements(element, index, array) {
+          data.xNumberOfBlocks.push(Number(element));
+        }
+        result.forEach(numberingArrayElements);
+        callback(err);
+      });
+    },
+    function (callback) {
+      client.lrange(pre_fix_chart.concat("BlockTime"), 0, -1, function (err, result) {
+        function numberingArrayElements(element, index, array) {
+          data.datasets[0].data.push(Number(element));
+        }
+        result.forEach(numberingArrayElements);
+        //console.log("data.datasets[0].data: " + data.datasets[0].data.length + " / " + result.length);
+        callback(err);
+      });
+    },
+    function (callback) {
+      client.lrange(pre_fix_chart.concat("Difficulty"), 0, -1, function (err, result) {
+        function numberingArrayElements(element, index, array) {
+          data.datasets[1].data.push(Number(element));
+        }
+        result.forEach(numberingArrayElements);
+        //console.log("data.datasets[1].data: " + data.datasets[1].data.length + " / " + result.length);
+        callback(err);
+      });
+    },
+    function (callback) {
+      client.lrange(pre_fix_chart.concat("NetHashrate"), 0, -1, function (err, result) {
+        function numberingArrayElements(element, index, array) {
+          data.datasets[2].data.push(Number(element));
+        }
+        result.forEach(numberingArrayElements);
+        //console.log("data.datasets[2].data: " + data.datasets[2].data.length + " / " + result.length);
+        callback(err);
+      });
+    },
+    function (callback) {
+      data.blockCount = data.dbLastBlock - data.dbChartLastBlock;
+      //console.log("blockCount: ", data.blockCount, "dbLastBlock: ", data.dbLastBlock, "dbChartLastBlock: ", data.dbChartLastBlock);
+      data.lastBlockTimes = 0;
+      dbSaveDatas.xData = [];
+      dbSaveDatas.BlockTime = [];
+      dbSaveDatas.xBlocknumber = [];
+      dbSaveDatas.xNumberOfBlocks = [];
+      dbSaveDatas.Difficulty = [];
+      dbSaveDatas.NetHashrate = [];
+
+      var cntDatasets = 0;
+      async.times(data.blockCount, function (n, next) {
+        //async.times(10000, function (n, next) {
+        var field = data.dbChartLastBlock + n;
+        if (field > 0) {
+          var fieldkey = pre_fix.concat((field - (field % divide)) + ":").concat(field);
+          client.hmget(fieldkey, 'timestamp', 'difficulty', 'number', function (err, block_info) {
+            if (err || !block_info) {
+              console.log(fieldkey + ": no block infomation");
+              next(err, null);
+            } else {
+              var baseOneTime = (60 * 60 * 1 * 1000);
+              var baseTime = (60 * 60 * 2 * 1000);
+              var hmgettimestamp = Number(block_info[0]) * 1000;
+              var nowTime = new Date();
+              var accNowTime = (nowTime - (nowTime % baseOneTime)) % baseTime == 0 ? (nowTime - (nowTime % baseOneTime)) - baseOneTime : (nowTime - (nowTime % baseOneTime));
+              if (accNowTime > hmgettimestamp) {
+                var hmgetdifficulty = Number(block_info[1]);
+                var hmgetnumber = Number(block_info[2]);
+                if (data.lastBlockTimes > 0) {
+                  var currentBlockTime = (hmgettimestamp - data.lastBlockTimes) / 1000;
+                  var currentDifficulty = hmgetdifficulty;
+                  var perSixHour = (hmgettimestamp - (hmgettimestamp % baseOneTime)) % baseTime == 0 ? (hmgettimestamp - (hmgettimestamp % baseOneTime)) - baseOneTime : (hmgettimestamp - (hmgettimestamp % baseOneTime));
+                  var idx = data.xData.indexOf(perSixHour);
+                  //console.log("nowTime:", nowTime.toLocaleString(), "accNowTime:", (new Date(accNowTime)).toLocaleString(), "hmgettimestamp:", (new Date(hmgettimestamp)).toLocaleString(), "perSixHour:", (new Date(perSixHour)).toLocaleString());
+
+                  if (idx == -1) {
+                    cntDatasets = 1;
+                    data.xData.push(perSixHour);
+                    data.xBlocknumber.push(hmgetnumber);
+                    data.xNumberOfBlocks.push(cntDatasets);
+                    tmpData.BlockTime.push(currentBlockTime);
+                    tmpData.Difficulty.push(currentDifficulty / 1000000000000);
+                    console.log("hmgetnumber:", hmgetnumber, "hmgettimestamp:", (new Date(hmgettimestamp)).toLocaleString(), "perSixHour:", (new Date(perSixHour)).toLocaleString());
+                    data.datasets[0].data.push(tmpData.BlockTime[data.xData.length - 1]);
+                    data.datasets[1].data.push(tmpData.Difficulty[data.xData.length - 1]);
+                    data.datasets[2].data.push((tmpData.Difficulty[data.xData.length - 1] / tmpData.BlockTime[data.xData.length - 1]) * 1000);
+
+                    data.lastnumber = hmgetnumber;
+                    dbSaveDatas.xData.push(perSixHour);
+                    dbSaveDatas.xBlocknumber.push(hmgetnumber);
+                    dbSaveDatas.xNumberOfBlocks.push(cntDatasets);
+                    dbSaveDatas.BlockTime.push(tmpData.BlockTime[data.xData.length - 1]);
+                    dbSaveDatas.Difficulty.push(tmpData.Difficulty[data.xData.length - 1]);
+                    dbSaveDatas.NetHashrate.push((tmpData.Difficulty[data.xData.length - 1] / tmpData.BlockTime[data.xData.length - 1]) * 1000);
+                  } else {
+                    cntDatasets++;
+                    data.xData[idx] = perSixHour;
+                    data.xBlocknumber[idx] = hmgetnumber;
+                    data.xNumberOfBlocks[idx] = cntDatasets;
+
+                    if (!tmpData.BlockTime[idx]) {
+                      tmpData.BlockTime[idx] = currentBlockTime;
+                    } else {
+                      tmpData.BlockTime[idx] += currentBlockTime;
+                    }
+                    if (!tmpData.Difficulty[idx]) {
+                      tmpData.Difficulty[idx] = currentDifficulty / 1000000000000;
+                    } else {
+                      tmpData.Difficulty[idx] += currentDifficulty / 1000000000000;
+                    }
+
+                    data.datasets[0].data[idx] = tmpData.BlockTime[data.xData.length - 1] / cntDatasets;
+                    data.datasets[1].data[idx] = tmpData.Difficulty[data.xData.length - 1] / cntDatasets;
+                    data.datasets[2].data[idx] = (data.datasets[1].data[idx] / data.datasets[0].data[idx]) * 1000;
+
+                    data.lastnumber = hmgetnumber;
+                    var dbsaveIdx = dbSaveDatas.xData.indexOf(perSixHour);
+                    dbSaveDatas.xData[dbsaveIdx] = perSixHour;
+                    dbSaveDatas.xBlocknumber[dbsaveIdx] = hmgetnumber;
+                    dbSaveDatas.xNumberOfBlocks[dbsaveIdx] = cntDatasets;
+                    dbSaveDatas.BlockTime[dbsaveIdx] = tmpData.BlockTime[data.xData.length - 1] / cntDatasets;
+                    dbSaveDatas.Difficulty[dbsaveIdx] = tmpData.Difficulty[data.xData.length - 1] / cntDatasets;
+                    dbSaveDatas.NetHashrate[dbsaveIdx] = (data.datasets[1].data[idx] / data.datasets[0].data[idx]) * 1000;
+                  }
+                }
+              }
+              if (hmgettimestamp > 0) {
+                data.lastBlockTimes = hmgettimestamp;
+              }
+              next(err, block_info);
+            }
+          });
+        } else {
+          next(null, null);
+        }
+      }, function (err, blocks) {
+        callback(err, blocks);
+      });
+    }
+  ], function (err, blocks) {
+    if (err) {
+      return next(err);
+    }
+
+    var multi = client.multi();
+    if (data.lastnumber > 0) {
+      multi.hset(pre_fix_chart.concat("lastblock"), "lastblock", data.lastnumber);
+    }
+    for (var i = 0; i < dbSaveDatas.xData.length; i++) {
+      multi.rpush(pre_fix_chart.concat('xData'), dbSaveDatas.xData[i]);
+      multi.rpush(pre_fix_chart.concat('xBlocknumber'), dbSaveDatas.xBlocknumber[i]);
+      multi.rpush(pre_fix_chart.concat('xNumberOfBlocks'), dbSaveDatas.xNumberOfBlocks[i]);
+      multi.rpush(pre_fix_chart.concat('BlockTime'), dbSaveDatas.BlockTime[i]);
+      multi.rpush(pre_fix_chart.concat('Difficulty'), dbSaveDatas.Difficulty[i]);
+      multi.rpush(pre_fix_chart.concat('NetHashrate'), dbSaveDatas.NetHashrate[i]);
+    }
+    multi.exec(function (errors, results) {
+      if (errors) {
+        console.log(errors);
+      }
+    });
+    dbSaveDatas = null;
+
+    //1) combine the arrays:
+    var list = [];
+    for (var j = 0; j < data.xData.length; j++) {
+      list.push({
+        'xData': data.xData[j],
+        'xBlocknumber': data.xBlocknumber[j],
+        'xNumberOfBlocks': data.xNumberOfBlocks[j],
+        'BlockTime': data.datasets[0].data[j],
+        'Difficulty': data.datasets[1].data[j],
+        'NetHashrate': data.datasets[2].data[j]
+      });
+    }
+
+    //2) sort:
+    list.sort(function (a, b) {
+      return ((a.xData < b.xData) ? -1 : ((a.xData == b.xData) ? 0 : 1));
+      //Sort could be modified to, for example, sort on the age 
+      // if the xData is the same.
+    });
+
+    //3) separate them back out:
+    for (var k = 0; k < list.length; k++) {
+      data.xData[k] = list[k].xData + (60 * 60 * 9 * 1000);
+      data.xBlocknumber[k] = list[k].xBlocknumber;
+      data.xNumberOfBlocks[k] = list[k].xNumberOfBlocks;
+      data.datasets[0].data[k] = list[k].BlockTime;
+      data.datasets[1].data[k] = list[k].Difficulty;
+      data.datasets[2].data[k] = list[k].NetHashrate;
+    }
+    /*
+    console.log("final data.xData: " + data.xData.length);
+    console.log("final data.xBlocknumber: " + data.xBlocknumber.length);
+    console.log("final data.xNumberOfBlocks: " + data.xNumberOfBlocks.length);
+    console.log("final data.datasets[0].data: " + data.datasets[0].data.length);
+    console.log("final data.datasets[1].data: " + data.datasets[1].data.length);
+    console.log("final data.datasets[2].data: " + data.datasets[2].data.length);
+    */
+    list = null;
+    tmpData = null;
+    res.render('hashratechart', {
+      xDataLength: JSON.stringify(data.xData.length + 1),
+      xData: JSON.stringify(data.xData),
+      xBlocknumber: JSON.stringify(data.xBlocknumber),
+      xNumberOfBlocks: JSON.stringify(data.xNumberOfBlocks),
+      BlockTime: JSON.stringify(data.datasets[0].data[k]),
+      Difficulty: JSON.stringify(data.datasets[1].data[k]),
+      NetHashrate: JSON.stringify(data.datasets[2].data[k]),
+      activity: JSON.stringify(data)
+    });
+    data = null;
+  });
+
+});
+
+module.exports = router;
